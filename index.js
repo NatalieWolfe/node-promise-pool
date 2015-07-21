060363b39b3a8ffcf6cbfb8dbc99b2b66de7456a
Natalie Wolfe <natalie@lifewanted.com>
2015-07-21 14:18:19 -0700

    Updated dox

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 
+/**
+ * The items exported by the `generic-promise-pool` package.
+ *
+ * @namespace exports
+ */
+
+/**
+ * The resource pooling class.
+ *
+ * @name exports.PromisePool
+ */
 var PromisePool = exports.PromisePool = require('./lib/PromisePool');
 
+/**
+ * Creates a new resource pool.
+ *
+ * @name exports.create
+ *
+ * @param {PromisePool.Factory} opts - Options for the new resource pool.
+ *
+ * @return {PromisePool} A new promise pool.
+ */
 exports.create = function createPool(opts){
     return new PromisePool(opts);
 };
