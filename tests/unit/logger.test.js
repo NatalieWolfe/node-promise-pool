998c02be7ba8b8c4e358cba782faf8caacc92a3f
Natalie Wolfe <natalie@lifewanted.com>
2016-09-11 20:54:34 -0700

    Added logger tests

---
+++ tests/unit/logger.test.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var should = require('should');
+var sinon = require('sinon');
+require('should-sinon');
+
+var logger = require('../../lib/logger');
+
+describe('logger', function() {
+    describe('.noop', function() {
+        it('should have all the required logging methods', function() {
+            _checkLoggingMethods(logger.noop);
+        });
+
+        it('should do nothing');
+    });
+
+    describe('.makeConsole', function() {
+        it('should return an object with the required logging methods', function() {
+            _checkLoggingMethods(logger.makeConsole('foo'));
+        });
+
+        describe('return value', function() {
+            /* eslint-disable no-console */
+            var originalLog = console.log;
+            var log = null;
+
+            beforeEach(function() {
+                console.log = sinon.spy();
+                log = logger.makeConsole('foo');
+            });
+
+            afterEach(function() {
+                console.log = originalLog;
+            });
+
+            it('should log to console.log', function() {
+                log.info('message');
+                console.log.should.be.calledWith('INFO', 'pool foo - message');
+            });
+            /* eslint-enable no-console */
+        });
+    });
+
+    describe('.fromFunction', function() {
+        it('should return an object with the required logging methods', function() {
+            _checkLoggingMethods(logger.fromFunction(function() {}));
+        });
+
+        describe('return value', function() {
+            var logSpy;
+            var log = null;
+
+            beforeEach(function() {
+                logSpy = sinon.spy();
+                log = logger.fromFunction('foo', logSpy);
+            });
+
+            it('should log to the provided function', function() {
+                log.info('message');
+                logSpy.should.be.calledWith('pool foo - message', 'info');
+            });
+        });
+    });
+});
+
+function _checkLoggingMethods(log) {
+    should.exist(log);
+    logger.LEVELS.forEach(function(level) {
+        log.should.have.property(level).which.is.a.Function();
+    });
+}
